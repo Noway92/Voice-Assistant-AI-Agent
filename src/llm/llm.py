374db9455c513db695748062b0863843112f87cd
Noé Le Yhuelic <noe.le_yhuelic@edu.devinci.fr>
2025-11-16 20:57:47 +0100

    Add: llm

---
+++ src/llm/llm.py
@@ -0,0 +1,25 @@
+from langchain_ollama import OllamaLLM
+from openai import OpenAI
+import os
+
+def llm_offline(input_text) :
+    print("Offline: ")
+    # Fontionnelle mais très long
+    llm = OllamaLLM(model="llama3")
+
+    # Faire une requête simple
+    response = llm.invoke(input_text)
+    print(response)
+
+
+llm_offline("Bonjour, tu vas bien ?")
+
+def llm_online(input_text: str, client: OpenAI = None):
+    print("Online: ")
+    if client is None:
+        client = OpenAI(api_key=os.environ["API_KEY_OPENAI"])
+    
+    response = client.responses.create(model="gpt-5",input=input_text)
+    print(response.output_text)
+
+#llm_online("Bonjour, tu vas bien ?")
